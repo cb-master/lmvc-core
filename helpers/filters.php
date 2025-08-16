f3480c738b9b137a3fbfcc625ec1eb4fe50fed45
cb-master <strblackhawk@gmail.com>
2025-08-17 00:48:50 +0600

    Helpers Added With Internal Functions & Filters

---
+++ helpers/filters.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Laika PHP MVC Framework
+ * Author: Showket Ahmed
+ * Email: riyadhtayf@gmail.com
+ * License: MIT
+ * This file is part of the Laika PHP MVC Framework.
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+// Load View
+add_filter('load_view', function(string $view){ echo $view; die; return trim($view); });
+
+// Load Style
+add_filter('load_style', function(string $style){ return trim($style); });
+
+// Load Script
+add_filter('load_script', function(string $script){ return trim($script); });
---
+++ helpers/functions.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Laika PHP MVC Framework
+ * Author: Showket Ahmed
+ * Email: riyadhtayf@gmail.com
+ * License: MIT
+ * This file is part of the Laika PHP MVC Framework.
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use CBM\Core\Filter;
+use CBM\Core\Uri;
+ 
+// Dump Data & Die
+/**
+ * @param mixed $data - Required Argument
+ * @param bool $die - Default is false
+ * @return void
+*/
+function dd(mixed $data, bool $die = false): void
+{
+    echo '<pre style="background-color:#000;color:#fff;">';
+    var_dump($data);
+    echo '</pre>';
+    $die ? die() : $die;
+}
+
+// Show Data & Die
+/**
+ * @param mixed $data - Required Argument
+ * @param bool $die - Default is false
+ * @return void
+*/
+function show(mixed $data, bool $die = false): void
+{
+    echo '<pre style="background-color:#000;color:#fff;">';
+    print_r($data);
+    echo '</pre>';
+    $die ? die() : $die;
+}
+
+// Redirect
+/**
+ * @param string|array $slug Required Argument
+ * @param ?array $params Optional Argument.
+ * @param int $response Optioanl parameter. Default is 302
+ * @return void
+*/
+function redirect(string|array $slug, ?array $params = null, int $response = 302): void
+{
+    // Convert to String if Slug is Array
+    if(is_array($slug)) $slug = implode('/', array_map('trim', $slug));
+    $slug = str_replace('\\', '/', $slug);
+    $slug = trim($slug, '/');
+
+    // Redirect
+    header('Location:'.Uri::build($slug, $params ?: []), true, $response);
+    die();
+}
+
+// Add Filter
+/**
+ * @param string $filter Required Argument.
+ * @param callable $callback Required Argument.
+ * @param int $priority Optional Argument. Default is 10
+ * @return void
+*/
+function add_filter(string $filter, callable $callback, int $priority = 10): void
+{
+    Filter::add_filter($filter, $callback, $priority);
+}
+
+// Apply Filter
+/**
+ * @param string $filter Required Argument.
+ * @param mixed $value Optional Argument. Default is Null.
+ * @param mixed ...$args Optional Arguments.
+ * @return mixed
+*/
+function apply_filter(string $filter, mixed $value = null, mixed ...$args): mixed
+{
+    return Filter::apply_filter($filter, $value, ...$args);
+}
