36171e6604eddc939caf4b95d62bb8509676c599
cb-master <strblackhawk@gmail.com>
2025-09-26 02:26:38 +0600

    Directory modified and File, Image, upload, Zip class added

---
+++ src/File.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ * Project: Laika MVC Framework
+ * Author Name: Showket Ahmed
+ * Author Email: riyadhtayf@gmail.com
+ */
+
+// Namespace
+namespace CBM\Core;
+
+use InvalidArgumentException;
+
+class File
+{
+    // Path
+    /**
+     * @var string $path
+     */
+    protected string $file;
+
+    public function __construct(string $file)
+    {
+        $this->file = $file;
+    }
+
+    ###############################################################
+    ## ----------------------- FILE INFO ----------------------- ##
+    ###############################################################
+    // Check Path Exist
+    public function exists(): bool
+    {
+        return file_exists($this->file);
+    }
+
+    // Check Path is Readable
+    public function readable(): bool
+    {
+        return is_readable($this->file);
+    }
+
+    // Check Path is Writable
+    public function writable(): bool
+    {
+        return is_writable($this->file);
+    }
+
+    // Get File Size
+    /**
+     * @return int|false Output will be in byte
+     */
+    public function size(): int|false
+    {
+        return $this->exists() ? filesize($this->file) : false;
+    }
+
+    // Get File Info
+    /**
+     * @return string Get File Info
+     */
+    public function info(): array
+    {
+        return pathinfo($this->file);
+    }
+
+    // Get Mime Type
+    /**
+     * @return string|false Mime Type of File on Success and false on Fail
+     */
+    public function mimeType(): string|false
+    {
+        return mime_content_type($this->file);
+    }
+
+    // Get Mime Extension
+    /**
+     * @return string Get Extension Name
+     */
+    public function extension(): string
+    {
+        return pathinfo($this->file, PATHINFO_EXTENSION);
+    }
+
+    // Get File Name
+    /**
+     * @return string Get File Name
+     */
+    public function name(): string
+    {
+        return pathinfo($this->file, PATHINFO_FILENAME);
+    }
+
+    // Get File Base Name
+    /**
+     * @return string Get File Name
+     */
+    public function base(): string
+    {
+        return pathinfo($this->file, PATHINFO_BASENAME);
+    }
+
+    // Get Path
+    /**
+     * @return string Path
+     */
+    public function path(): string
+    {
+        return pathinfo($this->file, PATHINFO_DIRNAME);
+    }
+
+    ######################################################################
+    ## --------------------- BASIC FILE OPERATION --------------------- ##
+    ######################################################################
+    /**
+     * Read File Content
+     * @return string|false
+     */
+    public function read(): string|false
+    {
+        return file_get_contents($this->file);
+    }
+
+    /**
+     * Write Content in File
+     * @param string $str Required Argument
+     * @return bool
+     */
+    public function write(string $str): bool
+    {
+        return $this->writable() ? (file_put_contents($this->file, $str) !== false) : false;
+    }
+
+    /**
+     * Add New Content in File
+     * @param string $str Content to append
+     * @return bool
+     */
+    public function append(string $str): bool
+    {
+        return $this->writable() ? (file_put_contents($this->file, $str, FILE_APPEND) !== false) : false;
+    }
+
+    /**
+     * Delete File
+     * @return bool
+     */
+    public function pop(): bool
+    {
+        return unlink($this->file);
+    }
+
+    // Move Path
+    /**
+     * Move File
+     * @param string $to New file name to move
+     * @return bool
+     */
+    public function move(string $to): bool
+    {
+        $result = rename($this->file, $to);
+        if ($result) $this->file = $to;
+        return $result;
+    }
+
+    /**
+     * Copy File
+     * @param string $to Destination File Name
+     * @return bool
+     */
+    public function copy(string $to): bool
+    {
+        return copy($this->file, $to);
+    }
+
+    /**
+     * Sets Access & Modification Time of File
+     * @param ?int $mtime Modefied Time. Default is null
+     * @param ?int $atime Access Time. Default is null
+     * @return bool
+     */
+    public function touch(?int $mtime = null, ?int $atime = null): bool
+    {
+        return touch($this->file, $mtime, $atime);
+    }
+
+    #########################################################################
+    ## -------------------------- File Download -------------------------- ##
+    #########################################################################
+    /**
+     * @param ?string $as Download As for Content Disposition
+     * @return void
+     */
+    public function download(?string $as = null): void
+    {
+        $filename = $as ?? $this->name();
+        $mime = $this->mimeType() ?: 'application/octet-stream';
+
+        header("Content-Type: {$mime}");
+        header("Content-Disposition: attachment; filename=\"{$filename}\"");
+        header("Content-Length: {$this->size()}");
+        readfile($this->file);
+        return;
+    }
+}
---
+++ src/Upload.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Laika PHP MVC Framework
+ * Author: Showket Ahmed
+ * Email: riyadhtayf@gmail.com
+ * License: MIT
+ * This file is part of the Laika PHP MVC Framework.
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+// Namespace
+namespace CBM\Core;
+
+class Upload
+{
+    // Fields
+    /**
+     * @var array $fields
+     */
+    protected $fields;
+
+    // Initialize Fields
+    public function __construct(array $fields)
+    {
+        $this->fields = $fields;
+    }
+
+    // Normalize Multiple Uploaded File
+    /**
+     * @param array $fileField File Field. Example $_FILES['field_name']
+     * @return array
+     */
+    public function normalize(): array
+    {
+        if (!isset($this->fields['name'])) return [];
+
+        if (!is_array($this->fields['name'])) return [$this->fields];
+
+        $files = [];
+        foreach ($this->fields['name'] as $i => $name) {
+            $files[] = [
+                'name'     => $this->fields['name'][$i],
+                'type'     => $this->fields['type'][$i],
+                'tmp_name' => $this->fields['tmp_name'][$i],
+                'error'    => $this->fields['error'][$i],
+                'size'     => $this->fields['size'][$i],
+            ];
+        }
+        return $files;
+    }
+
+    // Handle Uploaded File
+    /**
+     * @param string $directory Destination Directory
+     * @param ?string $name File Name Without Extension
+     * @return string|false
+     */
+    public function single(string $directory, ?string $name = null): string|false
+    {
+        if (!isset($this->fields['tmp_name']) || !is_uploaded_file($this->fields['tmp_name'])) return false;
+
+        // Make Directory if Not Exists
+        Directory::make($directory);
+
+        $originalName = basename($this->fields['name']);
+        $extension = pathinfo($originalName, PATHINFO_EXTENSION);
+        $name = $name ? $name . '.' . $extension : $originalName;
+
+        $destination = rtrim($directory, '/') . "/{$name}";
+
+        return move_uploaded_file($this->fields['tmp_name'], $destination) ? $destination : false;
+    }
+
+    // Handle Multiple Uploaded File
+    /**
+     * @param string $destinationDir Destination Directory
+     * @param array $options Optional Options. Example ['basename','maxsize','extensions','mimetypes']
+     * @return array
+     */
+    public function multiple(string $destinationDir, array $options = []): array
+    {
+        $results = ['success' => [], 'errors' => []];
+        $files = $this->normalize();
+
+        $baseName     = $options['basename'] ?? null;
+        $maxSize      = $options['maxsize'] ?? null;
+        $allowedExt   = isset($options['extensions']) ? array_map('strtolower', $options['extensions']) : null;
+        $allowedMime  = isset($options['mimetypes']) ? array_map('strtolower', $options['mimetypes']) : null;
+        $processImage = $options['image'] ?? false;
+
+        if (!is_dir($destinationDir)) {
+            mkdir($destinationDir, 0755, true);
+        }
+
+        foreach ($files as $index => $file) {
+            $name = $file['name'] ?? 'file_' . $index;
+            $size = $file['size'] ?? 0;
+            $tmp  = $file['tmp_name'] ?? '';
+            $error = $file['error'] ?? UPLOAD_ERR_NO_FILE;
+            // Get Extension
+            $ext = strtolower(pathinfo($name, PATHINFO_EXTENSION));
+            // Get Mime Info
+            $mime = '';
+            if($tmp && is_file($tmp)){
+                $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                $mime  = strtolower(finfo_file($finfo, $tmp));
+                finfo_close($finfo);
+            }
+            // Get Slug
+            $slug = self::slugify(pathinfo($name, PATHINFO_FILENAME));
+
+            if (empty($file['tmp_name']) || $file['error'] === UPLOAD_ERR_NO_FILE) {
+                continue; // Skip Blank/Empty Inputs
+            }
+
+            if (($error !== UPLOAD_ERR_OK) && $name) {
+                $results['errors'][$name] = "Upload error: {$error}";
+                continue;
+            }
+
+            if ($maxSize && ($size > (int) $maxSize)) {
+                $maxSizeMB = (int) $maxSize / 1024 / 1024;
+                $results['errors'][$name] = "File Exceeds Max Size ({$maxSizeMB} MB)";
+                continue;
+            }
+
+            if ($allowedExt && !in_array($ext, $allowedExt)) {
+                $results['errors'][$name] = "Extension .$ext not allowed";
+                continue;
+            }
+
+            if ($allowedMime && !in_array($mime, $allowedMime)) {
+                $results['errors'][$name] = "MIME type $mime not allowed";
+                continue;
+            }
+
+            $finalName = $baseName ? "{$baseName}_{$index}" : $slug;
+            $destination = rtrim($destinationDir, '/') . '/' . time() ."-{$finalName}.{$ext}";
+
+            if (move_uploaded_file($tmp, $destination)) {
+                if ($processImage && str_starts_with($mime, 'image/')) {
+                    $img = new Image($destination);
+                    $img->save($destination, 70);
+                    $img->destroy();
+                }
+
+                $results['success'][$name] = ['slug'=>basename($destination), 'path'=>$destination];
+            } else {
+                $results['errors'][$name] = "Could Not Move Uploaded File!";
+            }
+        }
+
+        return $results;
+    }
+
+    // Make File Name as Slug
+    /**
+     * @param string $text Text to Make Slug
+     * @return string
+     */
+    protected static function slugify(string $text): string
+    {
+        $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+        $text = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $text) ?: $text;
+        $text = preg_replace('~[^-\w]+~', '', $text);
+        $text = trim($text, '-');
+        $text = preg_replace('~-+~', '-', $text);
+        return strtolower($text) ?: 'file';
+    }
+}
